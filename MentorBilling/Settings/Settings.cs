70be889fae11c3fa96ee4fca94370eb8aa2070c0
DCW <DCW@DESKTOP-E78FF40>
2020-08-28 15:54:54 +0300

    Added Settings Class Added static database controller settings Added encrypted Database Settings in the appsettings.json

---
+++ MentorBilling/Settings/Settings.cs
@@ -0,0 +1,21 @@
+﻿using MentorBilling.Database.DatabaseController;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MentorBilling.Settings
+{
+    public class Settings
+    {
+#pragma warning disable IDE1006 // Naming Styles
+        private static DatabaseConnectionSettings databaseConnectionSettings { get; set; } = new DatabaseConnectionSettings();
+#pragma warning restore IDE1006 // Naming Styles
+
+        public static DatabaseConnectionSettings DatabaseConnectionSettings
+        {
+            get => databaseConnectionSettings;
+            set => databaseConnectionSettings = value;
+        }
+    }
+}
--- MentorBilling/Startup.cs
+++ MentorBilling/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MentorBilling.Data;
+using System.Net.Http;
 
 namespace MentorBilling
 {
@@ -29,6 +30,21 @@ namespace MentorBilling
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            // Server Side Blazor doesn't register HttpClient by default
+            if (!services.Any(x => x.ServiceType == typeof(HttpClient)))
+            {
+                // Setup HttpClient for server side in a client side compatible fashion
+                services.AddScoped<HttpClient>(s =>
+                {
+                    // Creating the URI helper needs to wait until the JS Runtime is initialized, so defer it.      
+                    var uriHelper = s.GetRequiredService<NavigationManager>();
+                    return new HttpClient
+                    {
+                        BaseAddress = new Uri(uriHelper.BaseUri)
+                    };
+                });
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
