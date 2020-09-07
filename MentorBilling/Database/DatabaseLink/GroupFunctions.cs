7612891a1eef4535f70e3bc8a74fdd142b3bd80c
DCW <DCW@DESKTOP-E78FF40>
2020-09-07 17:24:00 +0300

    Created a group structure

---
+++ MentorBilling/Database/DatabaseLink/GroupFunctions.cs
@@ -0,0 +1,27 @@
+﻿using MentorBilling.Database.DatabaseController;
+using MentorBilling.Login.UserControllers;
+using Npgsql;
+using System;
+
+namespace MentorBilling.Database.DatabaseLink
+{
+    public class GroupFunctions
+    {
+        /// <summary>
+        /// the database connection for the current class
+        /// </summary>
+        static readonly PosgreSqlConnection PgSqlConnection = new PosgreSqlConnection(Settings.Settings.DatabaseConnectionSettings);
+
+        public static void GetUserGroup(User user)
+        {
+            String queryCommand = "SELECT g.id AS id, g.denumire AS name, u.id AS admin_id," +
+                                    "u.nume_utilizator AS admin_username, u.nume AS admin_surname" +
+                                    "u.prenume AS admin_name " +
+                                    "FROM grupuri_utilizatori AS gu " +
+                                    "LEFT JOIN grupuri AS g ON gu.grup_id = g.id " +
+                                    "LEFT JOIN utilizatori AS u ON u.id = g.administrator_grup " +
+                                    "WHERE gu.utilizator_id = :p_user_id";
+            NpgsqlParameter queryParameter = new NpgsqlParameter("p_user_id", user.ID);
+        }
+    }
+}
--- MentorBilling/ObjectStructures/Group.cs
+++ MentorBilling/ObjectStructures/Group.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MentorBilling.Login.UserControllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,44 @@ namespace MentorBilling.ObjectStructures
 {
     public class Group
     {
+        #region Properties 
+        /// <summary>
+        /// the id property of the group structure
+        /// </summary>
+        private Int64 id { get; set; } = new Int64();
+        /// <summary>
+        /// the name of the group structure
+        /// </summary>
+        private String name { get; set; } = String.Empty;
+        //the administrator of the group
+        private User administrator { get; set; } = new User();
+        #endregion
+
+        #region Callers
+        /// <summary>
+        /// the main caller for the id property
+        /// </summary>
+        public Int64 ID
+        {
+            get => id;
+            set => id = value;
+        }
+        /// <summary>
+        /// the main caller for the name property
+        /// </summary>
+        public String Name
+        {
+            get => name;
+            set => name = value;
+        }
+        ///<summary>
+        ///the main caller for the administrator property
+        ///</summary>
+        public User Administrator
+        {
+            get => administrator;
+            set => administrator = value;
+        }
+        #endregion
     }
 }
