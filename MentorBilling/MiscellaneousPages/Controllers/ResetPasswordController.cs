46532f07b359f5127c22aab9a7cd0e7c176e9d07
DCW <DCW@DESKTOP-E78FF40>
2020-09-09 15:48:30 +0300

    Created a new controller for the password check

---
+++ MentorBilling/MiscellaneousPages/Controllers/ResetPasswordController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MentorBilling.MiscellaneousPages.Controllers
+{
+    public class ResetPasswordController
+    {
+        /// <summary>
+        /// the password bound to a given Password TextBox
+        /// </summary>
+        [Required(ErrorMessage = "Parola este camp obligatoriu.")]
+        public String Password { get; set; } = String.Empty;
+
+        /// <summary>
+        /// the password bound to a given Password TextBox
+        /// </summary>
+        [Required(ErrorMessage = "Parola este camp obligatoriu.")]
+        public String PasswordMatch { get; set; } = String.Empty;
+
+        /// <summary>
+        /// the errors for password does not match
+        /// </summary>
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Parola nu corespunde cu campul de verificare")]
+        public Boolean DoPasswordsMatch { get => Password == PasswordMatch; }
+    }
+}
