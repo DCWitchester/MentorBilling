c3c92cdd51682597c07a00fa3dd7444d4d85f9af
DCW <DCW@DESKTOP-E78FF40>
2020-09-23 17:36:09 +0300

    Altered the invoice controller slightly Started preparations for the generation of a new menu structure for the user

--- MentorBilling/Miscellaneous/Menu/Menu.cs
+++ MentorBilling/Miscellaneous/Menu/Menu.cs
@@ -7,6 +7,124 @@ namespace MentorBilling.Miscellaneous.Menu
 {
     public class Menu
     {
+        /// <summary>
+        /// the enum containing all the possible elements of the menu
+        /// </summary>
+        enum MenuItems
+        {
+            subscriptions = 0,
+            groups,
+            invoices,
+            settings
+        }
 
+        /// <summary>
+        /// the user menu generated for the specific user
+        /// </summary>
+        public List<MenuItem> UserMenu = new List<MenuItem>();
+
+        /// <summary>
+        /// the main Menu Initializer without any parameters
+        /// </summary>
+        public  Menu()
+        {
+            InitializeMenu();
+        }
+
+        /// <summary>
+        /// this function will initialize the UserMenu list
+        /// </summary>
+        void InitializeMenu()
+        {
+            //we will iterate through the possible values of the enum
+            foreach(MenuItems menuItems in Enum.GetValues(typeof(MenuItems)))
+            {
+                //for each possible value of the enum
+                //we add a new value
+                UserMenu.Add(
+                    //the newly added values are decided by the switch
+                    //for each possible value we call the linked function
+                    menuItems switch
+                    {
+                        MenuItems.subscriptions => InitializeSubscription(),
+                        MenuItems.groups => InitializeGroup(),
+                        MenuItems.invoices => InitializeInvoices(),
+                        MenuItems.settings => InitializeSettings(),
+                        //the default value is an empty item
+                        _ => InitializeVoidItem()
+                    }) ;
+            }
+        }
+
+        #region Item Initialization
+        /// <summary>
+        /// this function will return a subscription based menu item
+        /// </summary>
+        /// <returns>the menu item</returns>
+        MenuItem InitializeSubscription()
+        {
+            return new MenuItem()
+            {
+                //the id is set to the enum for the utmost control
+                MenuItemID = (Int32)MenuItems.subscriptions,
+                MenuDisplay = "Gestionare Abonamente",
+                IsActive = true
+            };
+        }
+
+        /// <summary>
+        /// this function will generate a group based menu item
+        /// </summary>
+        /// <returns>the menu item</returns>
+        MenuItem InitializeGroup()
+        {
+            return new MenuItem()
+            {
+                //the id is set to the enum for the utmost control
+                MenuItemID = (Int32)MenuItems.groups,
+                MenuDisplay = "Gestionare Grupuri",
+                IsActive = true
+            };
+        }
+
+        /// <summary>
+        /// this function will generate an invoice based menu item
+        /// </summary>
+        /// <returns>the menu item</returns>
+        MenuItem InitializeInvoices()
+        {
+            return new MenuItem()
+            {
+                //the id is set to the enum for the utmost control
+                MenuItemID = (Int32)MenuItems.invoices,
+                MenuDisplay = "Facturile Mele / Scadentar",
+                IsActive = true
+            };
+        }
+
+        /// <summary>
+        /// this function will generate a settings based menu item
+        /// </summary>
+        /// <returns>the menu item</returns>
+        MenuItem InitializeSettings()
+        {
+            return new MenuItem()
+            {
+                //the id is set to the enum for the utmost control
+                MenuItemID = (Int32)MenuItems.settings,
+                MenuDisplay = "Setari Facturare",
+                IsActive = true
+            };
+        }
+
+        /// <summary>
+        /// this function will generate an empty menu item
+        /// </summary>
+        /// <returns>the menu item</returns>
+        MenuItem InitializeVoidItem()
+        {
+            return new MenuItem();
+        }
+        #endregion
     }
 }
--- MentorBilling/Miscellaneous/Menu/MenuItem.cs
+++ MentorBilling/Miscellaneous/Menu/MenuItem.cs
@@ -7,7 +7,21 @@ namespace MentorBilling.Miscellaneous.Menu
 {
     public class MenuItem
     {
-        public Int32 MenuItemID { get; set; } = new Int32();
-        public String MenuDisplay { get; set; } = String.Empty;
+        /// <summary>
+        /// the Menu Item ID <= Preparation for later
+        /// </summary>
+        public Int32 MenuItemID { get; set; }
+        /// <summary>
+        /// the Menu Display used for the interface display 
+        /// </summary>
+        public String MenuDisplay { get; set; }
+        /// <summary>
+        /// the menu action called with object list and returning an object
+        /// </summary>
+        public Func<List<object>,object> MenuAction { get; set; }
+        /// <summary>
+        /// the menu Enabled Property display
+        /// </summary>
+        public Boolean IsActive { get; set; }
     }
 }
