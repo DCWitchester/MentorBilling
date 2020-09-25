1950bc58e911d5389a0c7b67d786b4a1b831410e
DCW <DCW@DESKTOP-E78FF40>
2020-09-25 17:23:59 +0300

    Commented the userMenu functionality

--- MentorBilling/Login/Pages/UserMenu.razor.cs
+++ MentorBilling/Login/Pages/UserMenu.razor.cs
@@ -15,25 +15,42 @@ namespace MentorBilling.Login.Pages
         [Parameter]
         public InstanceController InstanceController { get; set; } = new InstanceController();
 
+        /// <summary>
+        /// the visibility of the menu
+        /// </summary>
         Boolean IsMenuVisible { get; set; } = false;
 
+        /// <summary>
+        /// the display menu property
+        /// </summary>
         void DisplayMenu()
         {
             IsMenuVisible = !IsMenuVisible;
         }
 
+        /// <summary>
+        /// the initialization of the page calls the update of the menu from the database
+        /// </summary>
+        /// <returns>the initialization task back to the component</returns>
         protected override async Task OnInitializedAsync()
         {
+            //the update is done here and not at login for two main reasons
+            //REASON 1 : It matters not that it is here for this component is called at the exact moment of the login
+            //REASON 2 : The control is far better here than on the form
             await Task.Run(()=>MenuFunctions.UpdateLocalUserMenu(InstanceController.UserSettings.LoggedInUser, InstanceController.UserMenu));
         }
 
         void ExecuteMenuEvent(MenuItem menuItem)
         {
-            
+         //TODO: think about how to finish theevents on the button settings    
         }
 
+        /// <summary>
+        /// the main logout Action on the button click
+        /// </summary>
         void LogOut()
         {
+            //the main functionality is on the 
             MessageDisplay.CallLogoutWarning(InstanceController.MessageDisplaySettings);
         }
     }
