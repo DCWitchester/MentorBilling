d16291e9aff7bc88620e4b672ff467fd0a505891
DCW <DCW@DESKTOP-E78FF40>
2020-11-19 12:54:39 +0200

    Added needed packages for the creation of an entity context

---
+++ MentorBilling/ObjectStructures/Invoice/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MentorBilling.ObjectStructures.Invoice
+{
+    public class Product
+    {
+#pragma warning disable IDE1006 // Naming Styles
+        protected Int64 id { get; set; } = new Int64();
+        protected String productCode { get; set; } = String.Empty;
+        protected String productName { get; set; } = String.Empty;
+        protected String unitOfMeasure { get; set; } = String.Empty;
+        protected Double pricePerUnit { get; set; } = new Double();
+#pragma warning restore IDE1006 // Naming Styles
+
+    }
+}
