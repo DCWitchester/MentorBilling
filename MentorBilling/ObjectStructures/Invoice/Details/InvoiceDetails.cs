fa2f084831d2be46b604322d4d48598a64985db3
DCW <DCW@DESKTOP-E78FF40>
2020-11-20 16:25:20 +0200

    Added the procduct class and initialized the incoice details items

---
+++ MentorBilling/ObjectStructures/Invoice/Details/InvoiceDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MentorBilling.ObjectStructures.Invoice.Details
+{
+    public class InvoiceDetails : Product 
+    {
+        #region
+#pragma warning disable IDE1006 // Naming Styles
+        /// <summary>
+        /// the id property for the object
+        /// </summary>
+        protected Int64 id { get; set; } = new Int64();
+        /// <summary>
+        /// the quantity property for the object
+        /// </summary>
+        protected Double quantity { get; set; } = new Double();
+        /// <summary>
+        /// the unitary price for the object
+        /// </summary>
+        protected Double pricePerUnit { get; set; } = new Double();
+        /// <summary>
+        /// the discount for the object
+        /// </summary>
+        protected Double discount { get; set; } = new Double();
+#pragma warning restore IDE1006 // Naming Styles
+        #endregion
+    }
+}
---
+++ MentorBilling/ObjectStructures/Invoice/Details/Product.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MentorBilling.ObjectStructures.Invoice.Details
+{
+    public class Product
+    {
+        #region Properties
+#pragma warning disable IDE1006 // Naming Styles
+        /// <summary>
+        /// the id property for the object
+        /// </summary>
+        protected Int64 id { get; set; } = new Int64();
+        /// <summary>
+        /// the code property for the object
+        /// </summary>
+        protected String productCode { get; set; } = String.Empty;
+        /// <summary>
+        /// the name property for the object
+        /// </summary>
+        protected String name { get; set; } = String.Empty;
+        /// <summary>
+        /// the measurement unit for the object
+        /// </summary>
+        protected String unitOfMeasure { get; set; } = String.Empty;
+        /// <summary>
+        /// the price of an individual 
+        /// </summary>
+        protected Double pricePerUnit { get; set; } = new Double();
+        /// <summary>
+        /// the vat link for the object
+        /// </summary>
+        protected Int64 vatRate { get; set; } = new Int64();
+#pragma warning restore IDE1006 // Naming Styles
+        #endregion
+
+        #region Callers
+        /// <summary>
+        /// the caller for the id property
+        /// </summary>
+        public Int64 ID 
+        { 
+            get => id; 
+            set => id = value; 
+        }
+        /// <summary>
+        /// the caller for the product Code property
+        /// </summary>
+        public String ProductCode
+        {
+            get => productCode;
+            set => productCode = value;
+        }
+        /// <summary>
+        /// the caller for the name property
+        /// </summary>
+        public String Name
+        {
+            get => name;
+            set => name = value;
+        }
+        /// <summary>
+        /// the caller for the unit of measure property
+        /// </summary>
+        public String UnitOfMeasure
+        {
+            get => unitOfMeasure;
+            set => unitOfMeasure = value;
+        }
+        /// <summary>
+        /// the caller for the price per unit property
+        /// </summary>
+        public Double PricePerUnit
+        {
+            get => pricePerUnit;
+            set => pricePerUnit = value;
+        }
+        /// <summary>
+        /// the caller for the vat rate property
+        /// </summary>
+        public Int64 VATRate
+        {
+            get => vatRate; 
+            set => vatRate = value;
+        }
+        #endregion
+    }
+}
