bae552c854571a2f338bb9cf07da7f22b6e2c1bd
DCW <DCW@DESKTOP-E78FF40>
2020-11-20 13:20:40 +0200

    Ported UserLog to the EntityFramework

---
+++ MentorBilling/Database/EntityFramework/DatabaseLink/UserLog.cs
@@ -0,0 +1,26 @@
+﻿using MentorBilling.Database.EntityFramework.MentorBillingEntityFramework;
+using MentorBilling.Login.UserControllers;
+
+namespace MentorBilling.Database.EntityFramework.DatabaseLink
+{
+    public class UserLog : MentorBillingContext
+    {
+
+        /// <summary>
+        /// this function will login a given user in the log
+        /// </summary>
+        /// <param name="user">the user</param>
+        /// <returns>the state of the query</returns>
+        public void LoginUser(User user)
+        {
+            LogUtilizatori logUtilizator = new LogUtilizatori
+            {
+                UtilizatorId = user.ID,
+                Logged = true
+            };
+            base.LogUtilizatori.Add(logUtilizator);
+            base.SaveChanges();
+
+        }
+    }
+}
